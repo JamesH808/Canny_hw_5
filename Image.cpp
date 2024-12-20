8d0f7124195d65e4a55aa1e750d2eed83661bc26
JamesH808 <152203075+JamesH808@users.noreply.github.com>
2024-12-19 21:51:56 -0800

    Add files via upload

---
+++ Image.cpp
@@ -0,0 +1,34 @@
+//IMAGE struct for CENG 381 (adapted from UCI)
+//copy the following definition of IMAGE to your source code
+
+typedef struct Image_s
+{
+	unsigned char img[SIZE];
+
+	Image_s(void)
+	{
+	   for (int i=0; i<SIZE; i++)
+	   { 
+	      img[i] = 0;
+	   }
+	}
+
+	Image_s& operator=(const Image_s& copy)
+	{
+	   for (int i=0; i<SIZE; i++)
+	   { 
+	      img[i] = copy.img[i];
+	   }            
+	   return *this;
+	}
+
+	operator unsigned char*()
+	{
+	   return img;
+	}
+
+	unsigned char& operator[](const int index)
+	{
+	   return img[index];
+	}
+} IMAGE;
